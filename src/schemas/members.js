08264e119e667c670d02d4c4a5aee5d6d3bc7153
eliezer moo <fuuzze@gmail.com>
2022-04-17 19:41:41 -0500

    implementando validaciones

---
+++ src/schemas/members.js
@@ -0,0 +1,10 @@
+const Joi = require('@hapi/joi')
+
+const schema = Joi.object({
+    name: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required(),
+    birthDate: Joi.date().required()
+})
+
+module.exports = {  schema }
